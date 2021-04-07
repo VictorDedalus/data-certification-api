f3d8b9faeaa08b3386fe98072a8a044556a50855
Victor Lambert <lambertvictor24@gmail.com>
2021-04-07 18:01:03 +0200

    finished the api part

--- api/app.py
+++ api/app.py
@@ -1,4 +1,6 @@
 
+import pandas as pd
+import joblib
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -11,3 +13,50 @@ def index():
 
 
 # Implement a /predict endpoint
+@app.get("/predict/")
+def create(acousticness,
+			danceability,
+           	duration_ms,
+            energy,
+            explicit,
+            id,
+            instrumentalness,
+            key,
+            liveness,
+            loudness,
+            mode,
+            name,
+            release_date,
+            speechiness,
+            tempo,
+            valence,
+            artist):
+        X = pd.DataFrame(dict(
+            acousticness = [float(acousticness)],
+            danceability = [float(danceability)],
+            duration_ms = [int(duration_ms)],
+            energy = [float(energy)],
+            explicit = [int(explicit)],
+            id = [id],
+            instrumentalness = [float(instrumentalness)],
+            key = [int(key)],
+            liveness = [float(liveness)],
+            loudness = [float(loudness)],
+            mode = [int(mode)],
+            name = [name],
+            release_date = [release_date],
+            speechiness = [float(speechiness)],
+            tempo = [float(tempo)],
+            valence = [float(valence)],
+            artist = [artist]))
+            
+        pipeline = joblib.load('model.joblib')
+
+        results = pipeline.predict(X)
+
+        pred = float(results[0])
+
+        return dict(artist=artist,
+        name=name,
+        popularity=pred)
+            				
